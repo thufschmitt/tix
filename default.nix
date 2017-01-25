f9de009926c3a704ef5a657b8b8aeba773f92622
regnat <rg@regnat.ovh>
2017-01-25 09:38:28 +0000

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,20 @@
+{
+  nixpkgs ? <nixpkgs>, system ? builtins.currentSystem
+}:
+with import nixpkgs { inherit system; };
+ocamlPackages.buildOcaml rec {
+  name = "f_gadt";
+  version = "0.0";
+  shellHook = ''
+  export SHELL=$(${busybox}/bin/which zsh)
+  export REALRPROMPT=" — ${name}"
+  exec zsh
+  '';
+  createFindlibDestdir = false;
+  propagatedBuildInputs = [
+  ] ++ pkgs.envs.camlEnv;
+
+  src = ./.;
+}
+
+
