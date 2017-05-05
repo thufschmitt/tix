60f643851b2acc17cd7313989471dd21fb2661d2
regnat <rg@regnat.ovh>
2017-05-05 12:35:48 +0200

    nix-light: restore disabled tests

--- tests/nix_light.ml
+++ tests/nix_light.ml
@@ -38,8 +38,8 @@ let testsuite =
       "test_Y_comb", "(x: x x) (x: x x)", "(x: x x) (x: x x)";
       "test_annot", "(x /*: int */)", "(x /*: int */)";
       "test_annot_arrow", "(x /*: int -> int */)", "(x /*: (int) -> int */)";
-      (* "test_list", "Cons (1, Cons (2, Cons (3, nil)))", "Cons(1, Cons(2, Cons(3, nil)))"; *)
-      (* "test_list_sugar", "[1 2 3]", "Cons(1, Cons(2, Cons(3, nil)))"; *)
+      "test_list", "Cons (1, Cons (2, Cons (3, nil)))", "Cons(1, Cons(2, Cons(3, nil)))";
+      "test_list_sugar", "[1 2 3]", "Cons(1, Cons(2, Cons(3, nil)))";
     ] @
   [
   ]
