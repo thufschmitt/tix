96bc2d4f7fb2233e211dcd774726b47d8bc53db3
regnat <rg@regnat.ovh>
2017-05-05 15:11:13 +0200

    Fix the evaluation of typecheck's tests
    Some tests are still failing, but the testsuite now compiles

--- tests/test_typecheck.ml
+++ tests/test_typecheck.ml
@@ -1,14 +1,20 @@
 open OUnit2
 module T = Tix_types
 
+exception ParseError
+
 let get_type node =
   node.Onix_location.With_loc.description.Typed_ast.With_type.typ
 
 let test_typecheck_expr input expected_type _ =
   let tast =
-    Onix_parser.onix Onix_lexer.read (Lexing.from_string input)
-    |> Nl_of_onix.expr
-    |> Typecheck.expr Typing_env.empty
+    begin
+    match Onix_parser.onix Onix_lexer.read (Lexing.from_string input) with
+    | Some s ->
+      Nl_of_onix.expr s
+      |> Typecheck.expr Typing_env.empty
+    | None -> raise ParseError
+    end;
   in
   assert_equal
     expected_type
